8e183d99b19def7c6277093dc78b2b2288e5bbf3
Fred Jonsson <fridrik@pyth.net>
2013-03-25 21:59:46 +0000

    Initial commit (yo angular).

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularTableApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
